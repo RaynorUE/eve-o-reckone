f33521fe872e6affba24ea1eee73179aa70b689f
RaynorUE <raynorue@gmail.com>
2021-04-21 14:13:10 -0500

    whatever

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     alt = "logo" / >
     <
     p >
-    Edit < code > src / App.js < /code> and save to reload. wOooo <
+    Edit < code > src / App.js < /code> and save to reload. <
     /p> <
     a className = "App-link"
     href = "https://reactjs.org"
